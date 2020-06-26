cd994e223e45628a9109325b79fbe2b4e416506f
Chris Liang <christopherliang.cl@gmail.com>
2020-06-26 00:30:56 -0400

    Updated base file

--- Data_Collection.py
+++ Data_Collection.py
@@ -5,10 +5,11 @@ import gzip
 import json
 import csv
 import re
+import pandas as pd
 
 
 class NBAGame:
-    def __init__(self, game_id, json_file, seasonal_stats):  # object class used to store information regarding a specific NBA Game
+    def __init__(self, game_id, json_file, seasonal_stats):  # used to store information regarding a specific NBA Game
         self.game_id = game_id
         self.json_file = json_file
         self.seasonal_stats = seasonal_stats
@@ -58,16 +59,24 @@ class NBAGame:
         self.json_file = stats_in_game(self.game_id)  # uses game-specific box score JSON
         for player in self.json_file["resultSets"][0]["rowSet"]:  # increment through all players
             try:
-                if player[1] == self.home_team_id:  # add player to respective team
-                    self.home_team_players.append(self.seasonal_stats[str(player[4])])
+                mins_played = player[8]
+
+                if mins_played is None:
+                    mins_played = 0
                 else:
-                    self.away_team_players.append(self.seasonal_stats[str(player[4])])
+                    timestamp = re.match(r"(\d+):(\d+)", mins_played).groups()
+                    mins_played = round((int(timestamp[0]) * 60 + int(timestamp[1])) / 60, 1)
+
+                if mins_played > 0:
+                    seasonal_stats = self.seasonal_stats[str(player[5])]
+                    seasonal_stats.append(mins_played)
+                    if player[1] == self.home_team_id:  # add player to respective team
+                        self.home_team_players.append(seasonal_stats)
+                    else:
+                        self.away_team_players.append(seasonal_stats)
+
             except KeyError:
-                zero_player = [0] * 61
-                if player[1] == self.home_team_id:  # add player to respective team
-                    self.home_team_players.append(zero_player)
-                else:
-                    self.away_team_players.append(zero_player)
+                continue
 
     # compares scores and determines which team won
     def __set_result(self):
@@ -77,54 +86,71 @@ class NBAGame:
             self.home_win = False
 
     # prints all variables
-    def print(self):
+    def print_vars(self):
         attrs = vars(self)
         print('\n'.join("%s: %s" % item for item in attrs.items()))
 
-
     # inserts seasonal data into an array
     def compile_data(self):
-        game_data_array = []  # stores game statistics --> will be a row in the machine learning training file
-        stat_indexes = [0, 5, 6, 8, 9, 11, 12, 14, 15, 16, 18, 19, 20, 21, 23, 25, 26]  # indexes of statistics
-        stats_per_player = 17
+        game_data_array = []  # stores game statistics --> will be a row in the csv file
+        stat_indexes = [0, 63, 64, 5, 25, 6, 8, 9, 11, 12, 14, 15, 16, 18, 19, 20, 21, 23, 26, 47, 48]
+        edit_stat_indexes = [6, 9, 12, 15, 16, 18, 19, 20, 21, 23, 25, 26]  # indexes of stats to be modified
+        stats_per_player = 21
 
-        game_data_array.append(int(self.game_id))
+        game_data_array.append(int(self.game_id))  # adds gameID to array
+        game_data_array.append(int(self.home_win))  # adds win result to array
+
         # loops through all players on the home team and adds relevant data to array
         for i in range(len(self.home_team_players)):
-            if i > 12:
+            if i > 12:  # break if more than 13 players in list
                 break
-            for x in range(0, len(self.home_team_players[i])):
-                if x not in stat_indexes:
-                    continue
-                stat = self.home_team_players[i][x]
-                if stat is None:
-                    stat = 0
-                game_data_array.append(stat)
 
-        # fills in 0s if less than 13 players
-        if len(self.home_team_players) < 13:
+            mins_played = self.home_team_players[i][80]
+            mins_ratio = mins_played / self.home_team_players[i][5]
+
+            for x in stat_indexes:
+                if x is 5:
+                    stat = mins_played
+                else:
+                    stat = self.home_team_players[i][x]
+                    if stat is None:
+                        stat = 0
+
+                if x in edit_stat_indexes:
+                    game_data_array.append(round(stat * mins_ratio, 1))
+                else:
+                    game_data_array.append(stat)
+
+        if len(self.home_team_players) < 13:  # fills in 0s if less than 13 players
             missing_players = 13 - len(self.home_team_players)
             for i in range(missing_players * stats_per_player):
                 game_data_array.append(0)
 
         for i in range(len(self.away_team_players)):
-            if i > 12:
+            if i > 12:  # break if more than 13 players in list
                 break
-            for x in range(0, len(self.away_team_players[i])):
-                if x not in stat_indexes:
-                    continue
-                stat = self.away_team_players[i][x]
-                if stat is None:
-                    stat = 0
-                game_data_array.append(stat)
 
-        # fills in 0s if less than 13 players
-        if len(self.away_team_players) < 13:
+            mins_played = self.away_team_players[i][80]
+            mins_ratio = mins_played / (self.away_team_players[i][5])
+
+            for x in stat_indexes:
+                if x is 5:
+                    stat = mins_played
+                else:
+                    stat = self.away_team_players[i][x]
+                    if stat is None:
+                        stat = 0
+
+                if x in edit_stat_indexes:
+                    game_data_array.append(round(stat * mins_ratio, 1))
+                else:
+                    game_data_array.append(stat)
+
+        if len(self.away_team_players) < 13:  # fills in 0s if less than 13 players
             missing_players = 13 - len(self.away_team_players)
             for i in range(missing_players * stats_per_player):
                 game_data_array.append(0)
 
-        game_data_array.append(int(self.home_win))  # adds win result to array
         return game_data_array
 
 
@@ -163,11 +189,6 @@ def stats_in_game(game_id):
     json_file = json.loads(data)
     return json_file
 
-# gathers advanced stats for all the players in a given season
-# def get_advanced_stats(season):
-# #     jiebin can you write this method
-# #     just get all the basic advanced stats we will sort out which ones we want later
-
 
 # gathers seasonal stats for all players during a specified season
 def get_seasonal_stats(season):
@@ -183,9 +204,41 @@ def get_seasonal_stats(season):
 
     season_stats = {}
     for player in json_file["resultSets"][0]["rowSet"]:
-        player_id = str(player[0])
+        player_name = str(player[1])
         del player[0: 4]
-        season_stats[player_id] = player
+        del player[30:]
+        season_stats[player_name] = player
+
+    param = f"{season}-{season % 100 + 1}"
+    season_stats_url = f"https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Advanced&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season={param}&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight="
+    season_stats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Referer": "https://stats.nba.com/players/traditional/?sort=PTS&dir=-1", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+
+    req = urllib.request.Request(url=season_stats_url, headers=season_stats_headers)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data = str(gzip.decompress(data), 'utf-8')
+    json_file = json.loads(data)
+
+    for player in json_file["resultSets"][0]["rowSet"]:
+        player_name = str(player[1])
+        del player[0: 10]
+        del player[32:]
+        season_stats[player_name] = season_stats[player_name] + player
+
+    param = f"{season}-{season % 100 + 1}"
+    season_stats_url = f"https://stats.nba.com/stats/leaguedashplayerbiostats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season={param}&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+    season_stats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Referer": "https://stats.nba.com/players/traditional/?sort=PTS&dir=-1", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+
+    req = urllib.request.Request(url=season_stats_url, headers=season_stats_headers)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data = str(gzip.decompress(data), 'utf-8')
+    json_file = json.loads(data)
+
+    for player in json_file["resultSets"][0]["rowSet"]:
+        player_name = str(player[1])
+        del player[0: 5]
+        season_stats[player_name] = season_stats[player_name] + player
 
     return season_stats
 
@@ -197,59 +250,63 @@ def export_data(game_day_matrix, filename):
         csv_writer.writerows(game_day_matrix)  # writing the data rows
 
 
-def export_range(begin_month, begin_day, begin_year, end_month, end_day, end_year, filename):
-    if begin_month < 6:
-        seasonal_stats = get_seasonal_stats(begin_year-1)
+def collect_data(date_range, filename):
+    start_month = date_range[0].month
+    start_year = date_range[0].year
+    changed_season = True
+
+    if start_month > 4:
+        season = start_year
+        seasonal_stats = get_seasonal_stats(season)
     else:
-        seasonal_stats = get_seasonal_stats(begin_year)
-    for year in range(begin_year, end_year+1):
-        s_month = 1
-        e_month = 12
-        if year == begin_year:
-            s_month = begin_month
-        if year == end_year:
-            e_month = end_month
-        for month in range(s_month, e_month+1):
-            s_day = 1
-            e_day = 31
-            if month in range(5, 9):
-                continue
-            if month == 9:
-                seasonal_stats = get_seasonal_stats(year)
-                continue
-            if month == begin_month and year == begin_year:
-                s_day = begin_day
-            if month == end_month and year == end_year:
-                e_day = end_day
-            for day in range(s_day, e_day+1):
-                try:
-                    games = games_on_date(str(month).zfill(2), str(day).zfill(2), year)
-                    game_id_list = get_game_ids(games)
-                    game_day_matrix = []
-                    games_skipped = 0
+        season = start_year - 1
+        seasonal_stats = get_seasonal_stats(season)
 
-                    for game_id in game_id_list:
-                        if str(game_id)[2] is not "2":
-                            games_skipped += 1
-                            continue
-                        target_game = NBAGame(game_id, games, seasonal_stats)
-                        game_data = target_game.compile_data()
-                        game_day_matrix.append(game_data)
+    for date in date_range:
+        year = date.year
+        month = date.month
+        day = date.day
 
-                    time = datetime.datetime.now().strftime("%I:%M:%S %p")
-                    games_added = len(game_id_list) - games_skipped
+        if month in range(5, 10):
+            continue
 
-                    print(str(month).zfill(2), str(day).zfill(2), year, f": {games_added} games added\t({time})")
+        if month == 10 and changed_season is False:
+            season += 1
+            seasonal_stats = get_seasonal_stats(season)
+            changed_season = True
+        elif month != 10:
+            changed_season = False
 
-                    export_data(game_day_matrix, filename)
-                except HTTPError as ex:
-                    if ex.code == 400:
-                        break
-                    else:
-                        raise
+        try:
+            games = games_on_date(str(month).zfill(2), str(day).zfill(2), year)
+            game_id_list = get_game_ids(games)
+            game_day_matrix = []
+            games_skipped = 0
+
+            for game_id in game_id_list:
+                if str(game_id)[2] is not "2":
+                    games_skipped += 1
+                    continue
+                target_game = NBAGame(game_id, games, seasonal_stats)
+                game_data = target_game.compile_data()
+                game_day_matrix.append(game_data)
+
+            time = datetime.datetime.now().strftime("%I:%M:%S %p")
+            games_added = len(game_id_list) - games_skipped
+
+            export_data(game_day_matrix, filename)
+            print(str(month).zfill(2), str(day).zfill(2), year, f": {games_added} games added\t({time})")
+
+        except HTTPError as ex:
+            if ex.code == 400:
+                break
+            else:
+                raise
 
 
-# export_range(1, 10, 2016, 5, 1, 2017, csv_filename)
-# export_range(10, 27, 2015, 4, 10, 2019, csv_filename)
-csv_filename = "../Data/14-15_data.csv"
-export_range(10, 20, 2012, 4, 25, 2013, csv_filename)
+csv_filename = "../Data/10-11_data.csv"
+start_date = datetime.datetime(2010, 10, 20)
+end_date = datetime.datetime(2011, 4, 20)
+date_list = pd.date_range(start_date, end_date)
+collect_data(date_list, csv_filename)
+# print(get_seasonal_stats(2019))
