d3fcfe932210840cfd35565d2cc3269f94c446ab
Chris Liang <christopherliang.cl@gmail.com>
2020-07-03 02:33:09 -0400

    Finished collecting 14-15 data

--- Data_Collection.py
+++ Data_Collection.py
@@ -370,11 +370,8 @@ def collect_data(date_range, filename):
         exit()
 
 
-# csv_filename = "Data/13-14_data.csv"
-# start_date = datetime.datetime(2013, 11, 1)
-# end_date = datetime.datetime(2014, 4, 16)
 csv_filename = "Data/14-15_data.csv"
-start_date = datetime.datetime(2014, 2, 25)
+start_date = datetime.datetime(2015, 2, 25)
 end_date = datetime.datetime(2015, 4, 15)
 date_list = pd.date_range(start_date, end_date)
 collect_data(date_list, csv_filename)
