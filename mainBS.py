10e068cf3cb78d06ef14e9a3b6b9a0c297d4560e
anshulm07 <anshulmittal07@gmail.com>
2021-05-13 01:18:50 -0400

    Began reintegrating the database and form code into mainBS.py

--- mainBS.py
+++ mainBS.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from forms import PlayerSelectionForm
-
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select, and_
+import numpy as np
+import pickle
 
 # WebApp configuration and file paths
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ao19s2en1638nsh6msh172kd0s72ksj2'
+db = create_engine('sqlite:///NBAPlayers.db', echo=False)
+meta = MetaData()
+Pkl_Filename = "NBA_LRModel.pkl"
+with open(Pkl_Filename, 'rb') as file:
+    model = pickle.load(file)
+
+# Table format from database
+players = Table('v_players', meta,
+                Column('NAME', String),
+                Column('PLAYER_ID', String, primary_key=True),
+                Column('TEAM', String),
+                Column('TEAM_ID', String),
+                Column('YEAR', String, primary_key=True),
+                Column('AGE', Integer),
+                Column('HEIGHT', Integer),
+                Column('WEIGHT', Integer),
+                Column('MIN', Integer),
+                Column('PTS', Integer),
+                Column('FTM', Integer),
+                Column('FTA', Integer),
+                Column('FT_PCT', Integer),
+                Column('FGM', Integer),
+                Column('FGA', Integer),
+                Column('FG_PCT', Integer),
+                Column('FG3M', Integer),
+                Column('FG3A', Integer),
+                Column('FG3_PCT', Integer),
+                Column('AST', Integer),
+                Column('TOV', Integer),
+                Column('STL', Integer),
+                Column('BLK', Integer),
+                Column('OREB', Integer),
+                Column('DREB', Integer),
+                Column('PF', Integer),
+                Column('TEAM_NAME', String))
 
 
 # Route for webapp homepage - contains form
@@ -41,6 +78,60 @@ def model_page():
 @app.route('/about')
 def about_page():
     return render_template('aboutBS.html', title='About Us')
+
+
+# Queries database for list of names given the year - returns a JSON dictionary of objects containing player name and ID
+@app.route('/playerlist/<year>')
+def playerlist(year):
+    query = select([players.c.PLAYER_ID, players.c.TEAM, players.c.NAME]).where(players.c.YEAR == year)
+    conn = db.connect()
+    result = conn.execute(query)
+
+    player_array = []
+
+    for player in result:
+        playerObj = {}
+        playerObj["name"] = player.NAME + ", " + player.TEAM
+        playerObj["player_id"] = player.PLAYER_ID
+        player_array.append(playerObj)
+
+    return jsonify({"players": player_array})
+
+
+# Queries database for list of names given the team and year - returns a JSON dictionary of objects containing player name and ID
+@app.route('/autofill/<year>/<team_id>')
+def autofill(year, team_id):
+    query = select([players.c.PLAYER_ID, players.c.TEAM, players.c.NAME]).where(and_(players.c.YEAR == year, players.c.TEAM_ID == team_id)).order_by(players.c.MIN.desc()).limit(8)
+    conn = db.connect()
+    result = conn.execute(query)
+
+    player_array = []
+
+    for player in result:
+        playerObj = {}
+        playerObj["name"] = player.NAME + ", " + player.TEAM
+        playerObj["player_id"] = player.PLAYER_ID
+        player_array.append(playerObj)
+
+    return jsonify({"players": player_array})
+
+
+# Queries database for list of teams given the year - returns a JSON dictionary of objects containing team name and ID
+@app.route('/teamlist/<year>')
+def teamlist(year):
+    query = select([players.c.TEAM_ID, players.c.TEAM_NAME]).where(players.c.YEAR == year).distinct().order_by(players.c.TEAM_NAME)
+    conn = db.connect()
+    result = conn.execute(query)
+
+    team_array = []
+
+    for team in result:
+        teamObj = {}
+        teamObj["name"] = team.TEAM_NAME
+        teamObj["team_id"] = team.TEAM_ID
+        team_array.append(teamObj)
+
+    return jsonify({"teams": team_array})
 
 
 if __name__ == '__main__':
