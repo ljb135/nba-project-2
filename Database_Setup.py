30fa6d203b6b29b5ea6a2a0dc0c2dc19ff548b01
Chris Liang <christopherliang.cl@gmail.com>
2021-05-20 00:20:43 -0400

    Updated database setup

--- Database_Setup.py
+++ Database_Setup.py
@@ -141,44 +141,125 @@ def get_seasonal_stats(season):
                 del season_stats[player_name]
             continue
 
+    season_stats_url = f"https://stats.nba.com/stats/leaguehustlestatsplayer?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Advanced&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season={param}&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight="
+    season_stats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive",
+                            "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats",
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+                            "Referer": "https://stats.nba.com/players/traditional/?sort=PTS&dir=-1",
+                            "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+
+    req = urllib.request.Request(url=season_stats_url, headers=season_stats_headers)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data = str(gzip.decompress(data), 'utf-8')
+    json_file = json.loads(data)
+
+    for player in json_file["resultSets"][0]["rowSet"]:
+        if player[1] is None:
+            continue
+        player_name = str(player[1])
+        player = [player[10], player[16], player[8], player[9]]
+        try:
+            season_stats[player_name] = season_stats[player_name] + player
+        except Exception:
+            if player_name in season_stats:
+                del season_stats[player_name]
+            continue
+
+    season_stats_url = f"https://stats.nba.com/stats/leaguedashptdefend?College=&Conference=&Country=&DateFrom=&DateTo=&DefenseCategory=2+Pointers&Division=&DraftPick=&DraftYear=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season={param}&SeasonSegment=&SeasonType=Regular+Season&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+    season_stats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive",
+                            "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats",
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+                            "Referer": "https://stats.nba.com/players/traditional/?sort=PTS&dir=-1",
+                            "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+
+    req = urllib.request.Request(url=season_stats_url, headers=season_stats_headers)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data = str(gzip.decompress(data), 'utf-8')
+    json_file = json.loads(data)
+
+    for player in json_file["resultSets"][0]["rowSet"]:
+        if player[1] is None:
+            continue
+        player_name = str(player[1])
+        player = [player[9], player[10]]
+        try:
+            season_stats[player_name] = season_stats[player_name] + player
+        except Exception:
+            if player_name in season_stats:
+                del season_stats[player_name]
+            continue
+
+    season_stats_url = f"https://stats.nba.com/stats/leaguedashptdefend?College=&Conference=&Country=&DateFrom=&DateTo=&DefenseCategory=3+Pointers&Division=&DraftPick=&DraftYear=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season={param}&SeasonSegment=&SeasonType=Regular+Season&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+    season_stats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive",
+                            "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats",
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+                            "Referer": "https://stats.nba.com/players/traditional/?sort=PTS&dir=-1",
+                            "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+
+    req = urllib.request.Request(url=season_stats_url, headers=season_stats_headers)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data = str(gzip.decompress(data), 'utf-8')
+    json_file = json.loads(data)
+
+    for player in json_file["resultSets"][0]["rowSet"]:
+        if player[1] is None:
+            continue
+        player_name = str(player[1])
+        player = [player[9], player[10]]
+        try:
+            season_stats[player_name] = season_stats[player_name] + player
+        except Exception:
+            if player_name in season_stats:
+                del season_stats[player_name]
+            continue
+
     return season_stats
 
-# for year in range(2020, 2021):
-#     print(f"starting {year}")
-#     player_stats = get_seasonal_stats(year)
-#     for player in player_stats:
-#         stats = player_stats[player]
-#         query = players.insert().values(
-#             NAME=stats[2],
-#             PLAYER_ID=stats[1],
-#             TEAM=stats[4],
-#             TEAM_ID=stats[3],
-#             YEAR=stats[0],
-#             AGE=stats[5],
-#             HEIGHT=stats[24],
-#             WEIGHT=stats[25],
-#             MIN=stats[6],
-#             PTS=stats[23],
-#             FTM=stats[13],
-#             FTA=stats[14],
-#             FT_PCT=stats[15],
-#             FGM=stats[7],
-#             FGA=stats[8],
-#             FG_PCT=stats[9],
-#             FG3M=stats[10],
-#             FG3A=stats[11],
-#             FG3_PCT=stats[12],
-#             AST=stats[18],
-#             TOV=stats[19],
-#             STL=stats[20],
-#             BLK=stats[21],
-#             OREB=stats[16],
-#             DREB=stats[17],
-#             PF=stats[22],
-#             OFF_RTG=stats[26],
-#             DEF_RTG=stats[27],
-#             PACE=stats[28]
-#         )
-#         print("running query")
-#         conn = db.connect()
-#         result = conn.execute(query)
+for year in range(2020, 2021):
+    print(f"starting {year}")
+    player_stats = get_seasonal_stats(year)
+    for player in player_stats:
+        stats = player_stats[player]
+        query = players2.insert().values(
+            NAME=stats[2],
+            PLAYER_ID=stats[1],
+            TEAM=stats[4],
+            TEAM_ID=stats[3],
+            YEAR=stats[0],
+            AGE=stats[5],
+            HEIGHT=stats[24],
+            WEIGHT=stats[25],
+            MIN=stats[6],
+            PTS=stats[23],
+            FTM=stats[13],
+            FTA=stats[14],
+            FT_PCT=stats[15],
+            FGM=stats[7],
+            FGA=stats[8],
+            FG_PCT=stats[9],
+            FG3M=stats[10],
+            FG3A=stats[11],
+            FG3_PCT=stats[12],
+            AST=stats[18],
+            TOV=stats[19],
+            STL=stats[20],
+            BLK=stats[21],
+            OREB=stats[16],
+            DREB=stats[17],
+            PF=stats[22],
+            OFF_RTG=stats[26],
+            DEF_RTG=stats[27],
+            DEFL=stats[28],
+            LB_REC=stats[29],
+            CONT_2P=stats[30],
+            CONT_3P=stats[31],
+            DFG2M=stats[32],
+            DFG2A=stats[33],
+            DFG3M=stats[34],
+            DFG3A=stats[35])
+        print("running query")
+        conn = db.connect()
+        result = conn.execute(query)
