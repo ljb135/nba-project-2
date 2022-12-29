36c43f32779091600e2c3d0d53d15a53c968c85e
anshulm07 <anshulmittal07@gmail.com>
2022-12-29 00:49:03 -0500

    Updated database

--- Database_Setup.py
+++ Database_Setup.py
@@ -7,47 +7,74 @@ from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, 
 db = create_engine('sqlite:///NBAPlayers.db', echo=True)
 meta = MetaData()
 
-players2 = Table('players2', meta,
-                 Column('NAME', String),
+players3 = Table('players3', meta,
+                 Column('PLAYER_NAME', String),
                  Column('PLAYER_ID', String, primary_key=True),
-                 Column('TEAM_ABR', String),
                  Column('TEAM_ID', String),
-                 Column('YEAR', String, primary_key=True),
+                 Column('TEAM_ABBREVIATION', String),
+                 Column('SEASON', String, primary_key=True),
                  Column('AGE', Integer),
-                 Column('HEIGHT', Integer),
-                 Column('WEIGHT', Integer),
+                 Column('PLAYER_HEIGHT_INCHES', Integer),
+                 Column('PLAYER_WEIGHT', Integer),
+                 Column('DRAFT_YEAR', String),
+                 Column('DRAFT_ROUND', Integer),
+                 Column('DRAFT_NUMBER', Integer),
                  Column('GP', Integer),
                  Column('MIN', Integer),
                  Column('PTS', Integer),
-                 Column('FTM', Integer),
-                 Column('FTA', Integer),
-                 Column('FT_PCT', Integer),
+                 Column('EFG_PCT', Integer),
+                 Column('TS_PCT', Integer),
                  Column('FGM', Integer),
                  Column('FGA', Integer),
                  Column('FG_PCT', Integer),
                  Column('FG3M', Integer),
                  Column('FG3A', Integer),
                  Column('FG3_PCT', Integer),
+                 Column('FTM', Integer),
+                 Column('FTA', Integer),
+                 Column('FT_PCT', Integer),
+                 Column('OREB', Integer),
+                 Column('DREB', Integer),
+                 Column('REB', Integer),
                  Column('AST', Integer),
                  Column('TOV', Integer),
                  Column('STL', Integer),
                  Column('BLK', Integer),
-                 Column('OREB', Integer),
-                 Column('DREB', Integer),
                  Column('PF', Integer),
-                 Column('OFF_RTG', Integer),
-                 Column('DEF_RTG', Integer),
-                 Column('DEFL', Integer),
-                 Column('LB_REC', Integer),
-                 Column('CONT_2P', Integer),
-                 Column('CONT_3P', Integer),
-                 Column('DFG2M', Integer),
-                 Column('DFG2A', Integer),
-                 Column('DFG3M', Integer),
-                 Column('DFG3A', Integer))
+                 Column('PFD', Integer),
+                 Column('PLUS_MINUS', Integer),
+                 Column('OFF_RATING', Integer),
+                 Column('DEF_RATING', Integer),
+                 Column('NET_RATING', Integer),
+                 Column('AST_PCT', Integer),
+                 Column('AST_TO', Integer),
+                 Column('AST_RATIO', Integer),
+                 Column('OREB_PCT', Integer),
+                 Column('DREB_PCT', Integer),
+                 Column('REB_PCT', Integer),
+                 Column('USG_PCT', Integer),
+                 Column('PACE', Integer),
+                 Column('PIE', Integer),
+                 Column('POSS', Integer),
+                 Column('DEFLECTIONS', Integer),
+                 Column('CHARGES_DRAWN', Integer),
+                 Column('SCREEN_ASSISTS', Integer),
+                 Column('PTS_OFF_TOV', Integer),
+                 Column('PTS_2ND_CHANCE', Integer),
+                 Column('PTS_FB', Integer),
+                 Column('PTS_PAINT', Integer),
+                 Column('OPP_PTS_OFF_TOV', Integer),
+                 Column('OPP_PTS_2ND_CHANCE', Integer),
+                 Column('OPP_PTS_FB', Integer),
+                 Column('OPP_PTS_PAINT', Integer),
+                 Column('OPP_FGM', Integer),
+                 Column('OPP_FGA', Integer),
+                 Column('OPP_FG_PCT', Integer),
+                 Column('OPP_FG3M', Integer),
+                 Column('OPP_FG3A', Integer),
+                 Column('OPP_FG3_PCT', Integer))
 
-
-# meta.create_all(db)
+meta.create_all(db)
 
 # teams = Table('teams', meta,
 #                 Column('TEAM_ID', String),
@@ -144,7 +171,7 @@ def get_seasonal_stats(season):
     json_file = json.loads(data)
 
     stat_fields = {'PLAYER_ID', 'OFF_RATING', 'DEF_RATING', 'NET_RATING', 'AST_PCT', 'AST_TO', 'AST_RATIO', 'OREB_PCT',
-                   'DREB_PCT', 'REB_PCT', 'TM_TOV_PCT', 'EFG_PCT', 'TS_PCT', 'USG_PCT', 'PACE', 'PIE', 'POSS'}
+                   'DREB_PCT', 'REB_PCT', 'EFG_PCT', 'TS_PCT', 'USG_PCT', 'PACE', 'PIE', 'POSS'}
 
     headers = json_file["resultSets"][0]["headers"]
     stat_indexes = {stat: index for index, stat in enumerate(headers) if stat in stat_fields}
@@ -261,62 +288,91 @@ def get_seasonal_stats(season):
 
     return season_stats
 
-test_keys = ['SEASON', 'PLAYER_ID', 'PLAYER_NAME', 'TEAM_ID', 'TEAM_ABBREVIATION', 'AGE', 'GP', 'MIN', 'FGM', 'FGA',
+stat_names = ['SEASON', 'PLAYER_ID', 'PLAYER_NAME', 'TEAM_ID', 'TEAM_ABBREVIATION', 'AGE', 'GP', 'MIN', 'FGM', 'FGA',
                    'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'TOV',
                    'STL', 'BLK', 'PF', 'PFD', 'PTS', 'PLUS_MINUS', 'PLAYER_HEIGHT_INCHES', 'PLAYER_WEIGHT', 'DRAFT_YEAR', 'DRAFT_ROUND', 'DRAFT_NUMBER',
              'OFF_RATING', 'DEF_RATING', 'NET_RATING', 'AST_PCT', 'AST_TO', 'AST_RATIO', 'OREB_PCT',
-             'DREB_PCT', 'REB_PCT', 'TM_TOV_PCT', 'EFG_PCT', 'TS_PCT', 'USG_PCT', 'PACE', 'PIE', 'POSS',
+             'DREB_PCT', 'REB_PCT', 'EFG_PCT', 'TS_PCT', 'USG_PCT', 'PACE', 'PIE', 'POSS',
              'DEFLECTIONS', 'CHARGES_DRAWN', 'SCREEN_ASSISTS', 'PTS_OFF_TOV', 'PTS_2ND_CHANCE', 'PTS_FB', 'PTS_PAINT', 'OPP_PTS_OFF_TOV',
                    'OPP_PTS_2ND_CHANCE', 'OPP_PTS_FB', 'OPP_PTS_PAINT', 'OPP_FGM', 'OPP_FGA', 'OPP_FG_PCT', 'OPP_FG3M', 'OPP_FG3A', 'OPP_FG3_PCT']
 
-player_stats = get_seasonal_stats(2022)
-print(len(player_stats[203932]))
-print(dict(zip(test_keys, player_stats[203932])))
+# player_stats = get_seasonal_stats(2022)
+# print(len(player_stats[203932]))
+# print(dict(zip(test_keys, player_stats[203932])))
 
-# for year in range(2022, 2023):
-#     print(f"starting {year}")
-#     player_stats = get_seasonal_stats(year)
-#     for player in player_stats:
-#         stats = player_stats[player]
-#         if len(stats) == 37:
-#             query = players2.insert().values(
-#                 NAME=stats[2],
-#                 PLAYER_ID=stats[1],
-#                 TEAM_ABR=stats[4],
-#                 TEAM_ID=stats[3],
-#                 YEAR=stats[0],
-#                 AGE=stats[5],
-#                 HEIGHT=stats[25],
-#                 WEIGHT=stats[26],
-#                 GP=stats[6],
-#                 MIN=stats[7],
-#                 PTS=stats[24],
-#                 FTM=stats[14],
-#                 FTA=stats[15],
-#                 FT_PCT=stats[16],
-#                 FGM=stats[8],
-#                 FGA=stats[9],
-#                 FG_PCT=stats[10],
-#                 FG3M=stats[11],
-#                 FG3A=stats[12],
-#                 FG3_PCT=stats[13],
-#                 AST=stats[19],
-#                 TOV=stats[20],
-#                 STL=stats[21],
-#                 BLK=stats[22],
-#                 OREB=stats[17],
-#                 DREB=stats[18],
-#                 PF=stats[23],
-#                 OFF_RTG=stats[27],
-#                 DEF_RTG=stats[28],
-#                 DEFL=stats[29],
-#                 LB_REC=stats[30],
-#                 CONT_2P=stats[31],
-#                 CONT_3P=stats[32],
-#                 DFG2M=stats[33],
-#                 DFG2A=stats[34],
-#                 DFG3M=stats[35],
-#                 DFG3A=stats[36])
-#             print("running query")
-#             conn = db.connect()
-#             result = conn.execute(query)
+for year in range(2022, 2023):
+    print(f"starting {year}")
+    player_stats = get_seasonal_stats(year)
+    for player in player_stats:
+        stats = dict(zip(stat_names, player_stats[player]))
+        if len(stats) == 65:
+            query = players3.insert().values(
+                 PLAYER_NAME = stats['PLAYER_NAME'],
+                 PLAYER_ID = stats['PLAYER_ID'],
+                 TEAM_ID = stats['TEAM_ID'],
+                 TEAM_ABBREVIATION = stats['TEAM_ABBREVIATION'],
+                 SEASON = stats['SEASON'],
+                 AGE = stats['AGE'],
+                 PLAYER_HEIGHT_INCHES = stats['PLAYER_HEIGHT_INCHES'],
+                 PLAYER_WEIGHT = stats['PLAYER_WEIGHT'],
+                 DRAFT_YEAR = stats['DRAFT_YEAR'],
+                 DRAFT_ROUND = stats['DRAFT_ROUND'],
+                 DRAFT_NUMBER = stats['DRAFT_NUMBER'],
+                 GP = stats['GP'],
+                 MIN = stats['MIN'],
+                 PTS = stats['PTS'],
+                 EFG_PCT = stats['EFG_PCT'],
+                 TS_PCT = stats['TS_PCT'],
+                 FGM = stats['FGM'],
+                 FGA = stats['FGA'],
+                 FG_PCT = stats['FG_PCT'],
+                 FG3M = stats['FG3M'],
+                 FG3A = stats['FG3A'],
+                 FG3_PCT = stats['FG3_PCT'],
+                 FTM = stats['FTM'],
+                 FTA = stats['FTA'],
+                 FT_PCT = stats['FT_PCT'],
+                 OREB = stats['OREB'],
+                 DREB = stats['DREB'],
+                 REB = stats['REB'],
+                 AST = stats['AST'],
+                 TOV = stats['TOV'],
+                 STL = stats['STL'],
+                 BLK = stats['BLK'],
+                 PF = stats['PF'],
+                 PFD = stats['PFD'],
+                 PLUS_MINUS = stats['PLUS_MINUS'],
+                 OFF_RATING = stats['OFF_RATING'],
+                 DEF_RATING = stats['DEF_RATING'],
+                 NET_RATING = stats['NET_RATING'],
+                 AST_PCT = stats['AST_PCT'],
+                 AST_TO = stats['AST_TO'],
+                 AST_RATIO = stats['AST_RATIO'],
+                 OREB_PCT = stats['OREB_PCT'],
+                 DREB_PCT = stats['DREB_PCT'],
+                 REB_PCT = stats['REB_PCT'],
+                 USG_PCT = stats['USG_PCT'],
+                 PACE = stats['PACE'],
+                 PIE = stats['PIE'],
+                 POSS = stats['POSS'],
+                 DEFLECTIONS = stats['DEFLECTIONS'],
+                 CHARGES_DRAWN = stats['CHARGES_DRAWN'],
+                 SCREEN_ASSISTS = stats['SCREEN_ASSISTS'],
+                 PTS_OFF_TOV = stats['PTS_OFF_TOV'],
+                 PTS_2ND_CHANCE = stats['PTS_2ND_CHANCE'],
+                 PTS_FB = stats['PTS_FB'],
+                 PTS_PAINT = stats['PTS_PAINT'],
+                 OPP_PTS_OFF_TOV = stats['OPP_PTS_OFF_TOV'],
+                 OPP_PTS_2ND_CHANCE = stats['OPP_PTS_2ND_CHANCE'],
+                 OPP_PTS_FB = stats['OPP_PTS_FB'],
+                 OPP_PTS_PAINT = stats['OPP_PTS_PAINT'],
+                 OPP_FGM = stats['OPP_FGM'],
+                 OPP_FGA = stats['OPP_FGA'],
+                 OPP_FG_PCT = stats['OPP_FG_PCT'],
+                 OPP_FG3M = stats['OPP_FG3M'],
+                 OPP_FG3A = stats['OPP_FG3A'],
+                 OPP_FG3_PCT = stats['OPP_FG3_PCT']
+                )
+            print("running query")
+            conn = db.connect()
+            result = conn.execute(query)
